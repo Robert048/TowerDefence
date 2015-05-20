d9faa05d55c252ed079e8d69fcadb549c03c4e4f
Blindaxe <marco.t@live.nl>
2015-05-20 11:46:43 +0200

    canontower

---
+++ TowerDefence/TowerDefence/CanonTower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefense
+{
+    class CanonTower : Tower
+    {
+        public CanonTower(Texture2D tower)
+        {
+            damage = 20;
+            attackSpeed = 2;
+            range = 150;
+            ground = true;
+            air = false;
+            cost = 40;
+            towertexture = tower;
+
+        }
+    }
+}
