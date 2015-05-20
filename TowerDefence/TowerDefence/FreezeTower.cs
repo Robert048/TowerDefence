4058f40db557a17771f95ffe39581e68571ec6cf
Alex de Jonge <alex.de.jonge128@gmail.com>
2015-05-20 11:48:09 +0200

    Cost freezetower

---
+++ TowerDefence/TowerDefence/FreezeTower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefense
+{
+    class FreezeTower : Tower
+    {
+        protected int freezePower;
+
+        public FreezeTower(Texture2D freezeTower)
+        {
+            damage = 10;
+            attackSpeed = 1;
+            range = 100;
+            ground = true;
+            air = true;
+            cost = 50;
+            projectileList = new List<Projectile>();
+            towertexture = freezeTower;
+            freezePower = 20;
+        }
+    }
+}
