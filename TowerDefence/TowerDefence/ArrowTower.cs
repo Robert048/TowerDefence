cb987c44f8515d3d5d3d8abf3fdffdc9d3fc9478
Blindaxe <marco.t@live.nl>
2015-06-19 12:02:43 +0200

    arrowtower

--- TowerDefence/TowerDefence/ArrowTower.cs
+++ TowerDefence/TowerDefence/ArrowTower.cs
@@ -10,20 +10,19 @@ namespace TowerDefense
 {
     class ArrowTower : Tower
     {
-
+        // de arrowtower class waarin alle informatie van de arrowrtower staat
         public ArrowTower(Texture2D arrowTower, Texture2D arrow, Vector2 position)
         {
-            this.position = position;
-            damage = 5;
-            attackSpeed = 1000;
-            range = 200;
-            ground = true;
-            air = true;
-            cost = 20;
-            projectileList = new List<Projectile>();
-            texture = arrowTower;
-            projectileTexture = arrow;
-            towerString = "arrowTower";
+            this.position = position; //de positie waar de tower staat
+            damage = 5; //de schade dat de tower doet met elk projectiel
+            attackSpeed = 1000; //de snelheid waarmee de tower schiet in ms
+            range = 200; //de afstand waarin de toren kan schieten
+            air = true; //kan de toren luchtdoelen schieten
+            cost = 20; // hoeveel de toren kost om te plaatsen
+            projectileList = new List<Projectile>(); //lijst van geschoten projectielen
+            texture = arrowTower; //hoe de toren er uit ziet
+            projectileTexture = arrow; //hoe de projectiel er uit ziet
+            towerString = "arrowTower"; //de toren
         }      
     
     }
