3a3c237513ff0e9294ae13b0746a7301ddbb5a46
Blindaxe <marco.t@live.nl>
2015-06-19 11:57:53 +0200

    arrow comments

--- TowerDefence/TowerDefence/Arrow.cs
+++ TowerDefence/TowerDefence/Arrow.cs
@@ -10,15 +10,16 @@ namespace TowerDefense
 {
     class Arrow : Projectile
     {
+        //arrow class met alle informatie van de getekende arrow
         public Arrow(Texture2D arrow, int damage, Enemy target, Vector2 startPosition, int range)
         {
-            maxRange = range;
-            texture = arrow;
-            speed = 6;
-            this.damage = damage;
-            this.target = target;
-            position = startPosition;
-            projectileType = "arrow";
+            maxRange = range; //maximum afstand dat de arrow reist
+            texture = arrow; //hoe de arrow er uit ziet
+            speed = 6; // de snelheid waarmee de arrow beweegt
+            this.damage = damage; // de hoeveelheid schade de arrow doet wanneer die zijn doel raakt
+            this.target = target; // de doelwit van de arrow
+            position = startPosition; // vanaf welke toren de arow wordt afgeschoten
+            projectileType = "arrow"; //de type van de projectile
         }
     }
 }
